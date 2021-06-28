ad2b082c90d3a5bb240f52b94e38570c2b8ae189
Ignacio R. de Almeida <ignacio.rdea@gmail.com>
2021-06-28 14:27:20 -0600

    Initial commit

---
+++ src/hangman/EvilHangman.java
@@ -0,0 +1,13 @@
+package hangman;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+import java.util.SortedSet;
+
+public class EvilHangman{
+
+    public static void main(String[] args) {
+
+    }
+}
