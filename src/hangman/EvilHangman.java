229d9281696a32a7b237b6fd49a94e074f36bd4e
Ignacio R. de Almeida <57123713+IgnacioAlmeida@users.noreply.github.com>
2021-06-29 23:31:35 -0600

    Tests passed

--- src/hangman/EvilHangman.java
+++ src/hangman/EvilHangman.java
@@ -2,12 +2,93 @@ package hangman;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Set;
-import java.util.SortedSet;
+import java.util.*;
 
 public class EvilHangman{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws EmptyDictionaryException, IOException, GuessAlreadyMadeException {
+        String dictionary = args[0];
+        int wordLength = Integer.parseInt(args[1]);
+        int guesses = 0;
+        char guess = ' ';
+        StringBuilder key = new StringBuilder();
+        int limitGuesses = Integer.parseInt(args[2]);
+        Set<Character> usedGuesses = new TreeSet<>();
+
+        EvilHangmanGame newGame = new EvilHangmanGame();
+
+        Scanner input = new Scanner(System.in);
+        newGame.startGame(new File(dictionary), wordLength);
+
+        do{
+            if(limitGuesses > 1) {
+                System.out.printf("You have %s guesses left\n", limitGuesses - guesses);
+            }
+            else{
+                System.out.println("You have 1 guess left\n");
+            }
+
+            String lettersUsed = "Used letters: ";
+            for(Character c : usedGuesses){
+                lettersUsed += c + " ";
+            }
+            System.out.println(lettersUsed);
+
+            if(guesses == 0){
+                for(int i = 0; i < wordLength; i++){
+                    key.append('-');
+                }
+            }
+            System.out.println("Word: " + key);
+
+            boolean success = false;
+            while(!success) {
+                try {
+                    /*TODO TA QUESTIONS: Are we going to have specs on the prog exam? In this one, is
+                    it going to be about passing the tests or also about passing console input?*/
+                    System.out.println("Enter guess: ");
+                    guess = input.next().charAt(0);
+                    guess = Character.toLowerCase(guess);
+
+                    if(guess < 'a' || guess > 'z'){
+                        throw new IOException();
+                    }
+                    if (usedGuesses.contains(guess)) {
+                        throw new GuessAlreadyMadeException();
+                    }
+                    usedGuesses.add(guess);
+                    success = true;
+                } catch (GuessAlreadyMadeException ex) {
+                    System.out.println("Letter already entered, please try a new one: ");
+                } catch (IOException ex){
+                    System.out.println("Invalid input, please try again");
+                }
+            }
+            usedGuesses.add(guess);
+            newGame.makeGuess(guess);
+            guesses++;
+
+            //If the new key has a character, it adds it to the key in main
+//            if(guesses > 0){
+//                String temp;
+//                temp = newGame.getBiggestListKey().toString();
+//                for(int i = 0; i < temp.length(); i++){
+//                    if(temp.charAt(i) >= 'a' && temp.charAt(i) <= 'z'){
+//                        key.setCharAt(i, temp.charAt(i));
+//                    }
+//                }
+//            }
+//
+//            if(key.toString().indexOf('-') == - 1){
+//                System.out.println("You won! the word is: " + key.toString());
+//            }
+            if((limitGuesses - guesses) == 0){
+                System.out.println("You lose!");
+                System.out.println("The word was: " + newGame.randomWinWord());
+            }
+
+        }while(guesses < limitGuesses);
+
 
     }
 }
--- src/hangman/EvilHangmanGame.java
+++ src/hangman/EvilHangmanGame.java
@@ -2,22 +2,178 @@ package hangman;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Set;
-import java.util.SortedSet;
+import java.util.*;
 
 public class EvilHangmanGame implements IEvilHangmanGame{
+    private Set<String> englishWords = new HashSet<String>();
+    private SortedSet<Character> guessedLetters = new TreeSet<Character>();
+//    private String biggestListKey = "";
     @Override
     public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
+        Scanner scanner = new Scanner(dictionary);
+        englishWords.clear();
+        if(!scanner.hasNext()){
+            throw new EmptyDictionaryException();
+        }
+
+        while (scanner.hasNext()) {
+            EvilHangmanGame game = new EvilHangmanGame();
+            String str = scanner.next();
+            if (str.length() == wordLength) {
+                englishWords.add(str);
+            }
+        }
+
+        if(englishWords.size() == 0){
+            throw new EmptyDictionaryException();
+        }
+
+        scanner.close();
+        //if(englishword.empty() throw new
+
+        //Prints all the words with the size given
+//        for(String s : englishWords){
+//            System.out.println(s + "\n");
+//        }
 
     }
 
     @Override
     public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
-        return null;
+        guess = Character.toLowerCase(guess);
+
+        if(guessedLetters.contains(guess)){
+            throw new GuessAlreadyMadeException();
+        }
+        else {
+            guessedLetters.add(guess);
+        }
+
+        Map<String, Set<String>> keyedWords = new HashMap<>();
+        //System.out.println("Inside makeGuess function\n");
+
+        //Checks if guessed letter has been used already
+        if(guessedLetters.contains(guess)){
+
+        }
+
+        //3a
+        for(String string : englishWords){
+            StringBuilder key = new StringBuilder();
+
+            //Creates a key with the guessed letter for each letter in the dicctionary. Ex: Word Length 4,
+            //Guessed Letter: E, Key for ECHO would be E---
+            for(int i = 0; i < string.length(); i++){
+                key.append('-');
+                if(string.charAt(i) == guess){
+                    key.setCharAt(i, guess);
+                }
+            }
+            //System.out.printf("Key: %s \n", key);
+
+            //If that key isn't in the map, we create a set that has all the possible words that will match that key and
+            //assign it as the value. Ex: Key: E--- Values: ECHO, ESSO, etc
+            if(!(keyedWords.containsKey(key.toString()))){
+                Set<String> possibleWords = new HashSet<>();
+                possibleWords.add(string);
+                keyedWords.put(key.toString(), possibleWords);
+            }
+            //If the key already exists, we get the value related to that key which is a set of string and add the new
+            //word to the set
+            else{
+                keyedWords.get(key.toString()).add(string);
+            }
+        }
+
+        //3b Get the largest set of possibleWords from the map
+        String biggestListKey = "";
+        //Contains the set with the most words to make it harder to guess
+        Set<String> biggestList = new HashSet<>();
+        for(String key : keyedWords.keySet()){
+            if(biggestListKey.isEmpty()){
+                biggestListKey = key.toString();
+            }
+            else{
+                //Chooses the largest of the word groups
+                if(keyedWords.get(key.toString()).size() > keyedWords.get(biggestListKey).size()){
+                    biggestListKey = key.toString();
+                }
+                //.1 If they are of the same size, it chooses the one that has the guess char the least
+                if(keyedWords.get(key.toString()).size() == keyedWords.get(biggestListKey).size()){
+                    int keyGuessedLetterCount = 0;
+                    int biggestKeyGuessedLetterCount = 0;
+                    int keyIndexCount = 0;
+                    int biggestKeyIndexCount = 0;
+
+                    for(int i = 0; i < key.length(); i++){
+                        if(key.charAt(i) == guess){
+                            keyGuessedLetterCount++;
+                            keyIndexCount += i;
+                        }
+                        if(biggestListKey.charAt(i) == guess){
+                            biggestKeyGuessedLetterCount++;
+                            biggestKeyIndexCount += i;
+                        }
+                    }
+                    if(keyGuessedLetterCount < biggestKeyGuessedLetterCount){
+                        biggestListKey = key.toString();
+                    }
+
+                    //.2 if .1 is not solved, we choose the one with the rightmost guessed letter. Ex: E--E and -E-E, we
+                    //chose the second
+                    if(keyGuessedLetterCount == biggestKeyGuessedLetterCount){
+                        if(keyIndexCount > biggestKeyIndexCount){
+                            biggestListKey = key.toString();
+                        }
+                    }
+                    //if .2 is not solved, we choose the one with the next rightmost letter (repeat until group is chosen)
+                }
+            }
+        }
+
+        //it searches for the guess character in the key
+        biggestList = keyedWords.get(biggestListKey);
+        if(biggestListKey.indexOf(guess) == -1){
+            System.out.printf("Sorry, there are no %s's\n\n", guess);
+        }
+
+        if(biggestListKey.indexOf(guess) != -1){
+            int counter = 0;
+            for(int i = 0; i < biggestListKey.length(); i++){
+                if(biggestListKey.charAt(i) == guess){
+                    counter++;
+                }
+            }
+            System.out.printf("Yes, there is %s %s's\n\n",counter, guess);
+        }
+
+//        String lettersUsed = "Used letters: ";
+//        for(Character c : guessedLetters){
+//            lettersUsed += c + " ";
+//        }
+//        System.out.println(lettersUsed);
+//
+//        System.out.printf("Word: %s\n", biggestListKey);
+
+        englishWords = biggestList;
+
+        return biggestList;
     }
 
     @Override
     public SortedSet<Character> getGuessedLetters() {
-        return null;
+        return guessedLetters;
     }
+
+    public String randomWinWord(){
+        String[] randomWord = englishWords.toArray(new String[englishWords.size()]);
+        Random rndm = new Random();
+        int rndmNumber = rndm.nextInt(englishWords.size());
+        String output = randomWord[rndmNumber];
+        return output;
+    }
+
+//    public String getBiggestListKey(){
+//        return biggestListKey;
+//    }
 }
